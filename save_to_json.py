7535f0b33be3679b72444f52215fd9252b014d1b
neerajs6 <64753404+neerajs6@users.noreply.github.com>
2020-05-21 12:18:39 -0400

    Save tokenized text files to json

---
+++ save_to_json.py
@@ -0,0 +1,47 @@
+import sys
+import os
+import hashlib
+import subprocess
+import collections
+
+import jso
+import tarfile
+import io
+import pickle as pkl
+
+dm_single_close_quote = '\u2019' # unicode
+dm_double_close_quote = '\u201d'
+
+def convert_txt_json():
+	# loop over all story files in directory
+	#for file in os.listdir(r'/Users/neerajsudhakar/Documents/ByteSizeArxixv-master/library/toToken/0001004v1.txt'):
+    file = r'/Users/neerajsudhakar/Desktop/0001004v1.txt'
+    # empty lists for each story
+    abstract_lines = list()
+    article_lines = list()
+    with open(file,'r') as f:
+        hashfound = False
+        for line in f.readlines():
+            line=line.rstrip('\n').replace(dm_single_close_quote, '\'').replace(dm_double_close_quote, '\"')
+            if line == '#####':
+                hashfound = True
+            elif hashfound:
+                article_lines.append(line)
+            else:
+                abstract_lines.append(line)
+    
+    fileid=file.split('/')
+    fileid=fileid[len(fileid)-1].replace('.txt', '')
+		
+    # make json data
+    js_example = {}
+    js_example['id'] = fileid
+    js_example['article'] = article_lines
+    js_example['abstract'] = abstract_lines
+
+
+    with open(file.replace('.txt', '.json') , 'w') as outfile:
+        json.dump(js_example, outfile)
+
+#if __name__ == "__main__":
+convert_txt_json()
